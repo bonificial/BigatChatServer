096a9f1c04887c0af003c681b8db27727b7c155d
bonificial <mboniface107@gmail.com>
2021-07-18 16:26:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: [ "GET", "POST" ]
+    }
+})
+
+io.on("connection", (socket) => {
+    let customID = socket.handshake.query.CustomId;
+
+    socket.emit("me",  socket.id);
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser", (data) => {
+       console.log('Incming call with data ', {...data,signalData:{}})
+        io.to(socket.id).emit('callResponse', 'call response initiated');
+        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+    })
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    })
+})
+
+server.listen(process.env.PORT || 5000, () => console.log("server is running on port 5000"))
